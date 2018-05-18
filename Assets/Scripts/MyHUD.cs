889108e14062097b0437a20f820aa8b1c18dd67b
DESKTOP-IIM4DVV\CAP <allenrader2002@gmail.com>
2018-05-18 12:43:45 -0500

    Replacing Network Manager HUD

---
+++ Assets/Scripts/MyHUD.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyHUD : MonoBehaviour {
+
+	private NetworkManager networkManager;
+
+	// Use this for initialization
+	void Start () {
+		networkManager = GetComponent<NetworkManager> ();
+	}
+
+	public void MyStartHost () {
+		networkManager.StartHost ();
+		Debug.Log ("Starting host at " + Time.timeSinceLevelLoad);
+	}
+	
+	void OnStartHost () {
+		Debug.Log ("Host started at " + Time.timeSinceLevelLoad);
+	}
+}
