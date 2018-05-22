27c38d376a4a981989984e496959d9537681ee88
DESKTOP-IIM4DVV\CAP <allenrader2002@gmail.com>
2018-05-22 10:54:47 -0500

    Networked Player Movement pt.1

---
+++ Assets/Players/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+	void Move () {
+		float h = CrossPlatformInputManager.GetAxis ("Horizontal");
+		float v = CrossPlatformInputManager.GetAxis ("Vertical");
+
+		transform.Translate (new Vector3 (h, 0, v));
+
+	}
+}
